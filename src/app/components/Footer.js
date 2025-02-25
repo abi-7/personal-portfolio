75b45921bed10548f2076799b2025dfac9eba0c9
Abigail <abigailf03@hotmail.ca>
2025-02-24 23:27:06 -0600

    added a footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+
+
+export default function Footer() {
+  
+  return (
+    <div className="flex flex-col items-center pt-[150px]">
+
+      <div className="text-center">
+        <p className="text-gray-600">
+          Developed and designed by Abigail Ferreira.
+        </p>
+        <p className="text-gray-600 mb-10">
+         2025
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import HeroSection from "./components/Hero";
 import AboutSection from "./components/About";
 import ProjectsSection from "./components/Projects";
 import ExperienceSection from "./components/Experience";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -19,6 +20,9 @@ export default function Home() {
       </section>
       <section id="experience">
         <ExperienceSection />
+      </section>
+      <section>
+        <Footer />
       </section>
     </>
   );
