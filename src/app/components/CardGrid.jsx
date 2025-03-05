6ea64fe7a41a4ccb38c42b185f780312bbfa8ad7
Abigail <abigailf03@hotmail.ca>
2025-03-05 12:05:46 -0600

    added new .svg icons to page & added project links

--- src/app/components/CardGrid.jsx
+++ src/app/components/CardGrid.jsx
@@ -11,13 +11,15 @@ import { motion } from 'framer-motion';
 const cards = [
   {
     id: 1,
+    svg: '/pixel-guy.svg',
     title: 'Interactive about me page',
     description:
       'A playful and creative way to showcase my day-to-day life. Users can navigate a virtual house, interact with objects, and learn more about me through dialogue prompts.',
-    link: 'https://github.com/abi-7/2D-game',
+    link: 'https://day-in-pixels.netlify.app/',
   },
   {
     id: 2,
+    svg: '/vase.svg',
     title: 'Kiln Connection',
     description:
       'An immersive web platform connecting professional artists and pottery enthusiasts, "Kiln Connection" was created as my final project for a Web Development course using php.',
@@ -25,13 +27,15 @@ const cards = [
   },
   {
     id: 3,
+    svg: '/guess.svg',
     title: 'Character Guessing Game',
     description:
       'This project is a fun and interactive guessing game based on Rick & Morty characters! Players are shown a random character image alongside four name options from the show and must guess the correct match.',
-    link: 'https://github.com/abi-7/character-game',
+    link: 'https://rick-n-morty-guess.netlify.app/',
   },
   {
     id: 4,
+    svg: '/coffee.svg',
     title: 'Coffee.Break',
     description:
       'A fun desktop application that alerts you when it is time for your coffee break! (creative implementation of a pomodoro timer)',
@@ -50,19 +54,11 @@ export function CardWithLink() {
         >
           <Card className="h-full flex flex-col">
             <CardBody className="flex-grow">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
+              <img
+                src={card.svg}
+                alt={card.title}
                 className="mb-4 h-12 w-12 text-gray-900"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-                  clipRule="evenodd"
-                />
-                <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
-              </svg>
+              />
               <Typography variant="h5" color="blue-gray" className="mb-2">
                 {card.title}
               </Typography>
